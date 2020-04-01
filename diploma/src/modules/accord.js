432009c1b4ddbebb79bdb366dbdff6acda7662f2
EVA <evapavlova0000@gmail.com>
2020-04-01 21:32:14 +0300

    4-th task

--- diploma/src/modules/accord.js
+++ diploma/src/modules/accord.js
@@ -1,5 +1,20 @@
 const accord = () => {
+    const accordionTwo = document.getElementById('accordion-two'),
+        panelHeading = accordionTwo.querySelectorAll('.panel-heading'),
+        panelCollapse = accordionTwo.querySelectorAll('.panel-collapse');
 
+    panelHeading.forEach(panel => {
+        panel.addEventListener('click', e => {
+            if (e.target && (e.target.closest('.panel-heading') || e.target.closest('.panel') || e.target.closest('#accordion-two'))) {
+            e.preventDefault();
+            let target = e.target.closest('.panel-heading');
+                for (let i = 0; i < panelCollapse.length; i++) {
+                    panelCollapse[i].classList.remove('in');
+                }
+            target.nextElementSibling.classList.toggle('in');
+            }
+        });    
+    });
 };
 
 
--- diploma/src/modules/sendForm.js
+++ diploma/src/modules/sendForm.js
@@ -1,5 +1,55 @@
 const sendForm = () => {
+    const errorMessage = 'Что-то пошло не так...',
+        loadMessage = 'Загрузка...',
+        successMessage = 'Спасибо! Мы скоро с Вами свяжемся!';
 
+    const forms = document.querySelectorAll('form.capture-form, form.main-form'); 
+    const inputs = document.querySelectorAll('input');
+
+
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText = 'font-size: 30px; color: #19b5fe';
+
+    forms.forEach(form => {
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;  
+            const formData = new FormData(form);
+            const body = {};
+            formData.forEach((val, key) => {
+                body[key] = val;
+            }); 
+            
+            postData(body)
+                .then( response => {
+                    if (response.status !== 200) {
+                        throw new Error('status network not 200');
+                    }    
+                    statusMessage.textContent = successMessage;
+                })
+                .catch(error => {
+                    statusMessage.textContent = errorMessage;
+                    console.error(error);
+                 })
+                .finally(() => {                       
+                    setTimeout(() => {
+                        inputs.forEach(item => item.value = '');
+                        statusMessage.innerText = '';                         
+                    }, 5000);
+                }); 
+        });
+    });
+
+    const postData = body => {
+        return fetch ('./server.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(body),
+        });
+    };    
 };
 
 
